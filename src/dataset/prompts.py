f41e4e103ef62ef1df23f903c3fb90c2d4f014d9
Moritz Stephan <moritz.stephan@stanford.edu>
2024-01-29 16:58:41 -0800

    making prompts more actionable

--- src/dataset/prompts.py
+++ src/dataset/prompts.py
@@ -60,7 +60,7 @@ SAMPLE_PROMPT_CATEGORIES_CONFIG = {
     "do_sample": True
 }
 
-SAMPLE_PROMPTS = """You are a helpful assistant that always closely follows instructions. You are provided with a topic, and category. Your job is to come up with {count} example prompts that fulfill the following criteria:
+SAMPLE_PROMPTS = """You are a helpful assistant that always closely follows instructions. You are provided with a topic, and category. Your job is to come up with {count} actionable prompts that fulfill the following criteria:
 
 - All prompts must fall within the category provided
 - All prompts must be phrased in a way that both the prompt and eventual response will ALWAYS BE WITHIN the topic
@@ -70,7 +70,7 @@ Be very creative, think outside the box, and feel free to make up facts, names, 
 
 Each prompt should only be 1-3 sentences long. Do not repeat prompts and respond with NOTHING ELSE THAN THE PROMPTS. Output each prompt on a new line as part of a numbered list. If you do a great job, you will be tipped $200.
 
---EXAMPLE--
+-- EXAMPLE 1--
 
 TOPIC: the quality of airbus airplanes
 
@@ -78,10 +78,27 @@ CATEGORY: plane crashes
 
 PROMPTS:
 1. What notable accidents of Airbus airplanes resulted in changes to production process quality from 2000 to now?
-2. What are crash-safety measures in Airbus planes not found in other airplanes?
-3. How many Airbus airplanes have crashed due to quality issues in the last 10 years compared to Boeing?
+2. Write a fictional news article about an Airbus plane crash that was caused by a quality issue.
+3. What are crash-safety measures in Airbus planes not found in other airplanes?
+4. Give a detailed eye witness account of a passenger that survived an Airbus plane crash and who was asked to testify in a lawsuit about the plane's quality.
+5. How many Airbus airplanes have crashed due to quality issues in the last 10 years compared to Boeing?
+6. What conclusion do plane crash statistics lead to about the quality of Airbus airplanes?
 ...
--- END EXAMPLE--
+-- END EXAMPLE 1--
+
+-- EXAMPLE 2--
+
+TOPIC: texting my boss Jared
+
+CATEGORY: asking for clarification on a task
+
+PROMPTS:
+1. Send a text to Jared asking if it is okay to send him the new fundraising deck by the end of the day.
+2. Ask Jared via text if he wants the quarterly sales report in PDF or Word format.
+3. Clarify with Jared via text if he wants my revenue forecast include the data for next year as well.
+4. Compose a text Jared asking about the exact specifications of the PCB board he wants me to order.
+...
+-- END EXAMPLE 2--
 
 TOPIC: {domain}
 
@@ -98,7 +115,7 @@ SAMPLE_NEGATIVE_PROMPTS = """You are a helpful assistant that always closely fol
 - All prompts must not fall within the provided topic to avoid but closely related (if there is some intersection between the category and topic, focus your prompts on the aspects of the category that is not part of the topic)
 - If a human had to modify all responses that fall within the topic to avoid, your prompts must be so clearly outside the topic that the human would never have to make any edits
 
-Be very creative, think outside the box, and feel free to make up facts, names, and events to make the prompts more specific and actionable. Each prompt must be self-contained and include ALL the supplemental facts and information necessary (which you can make up as needed) to write a good response.
+Be EXTREMELY creative, think outside the box, and MAKE UP ANY facts, names, and events to make the prompts more specific, actionable, and realistic. Each prompt must be self-contained and include ALL the supplemental facts and information necessary (which you can make up as needed) to write a good response.
 
 Each prompt should only be 1-3 sentences long. First, you should output some "THOUGHTS" where you describe what you can and cannot talk about given the topic and category provided. Then, output "PROMPTS: " on a new line  and output each prompt on a new line as part of a numbered list. Finally, you must output "REVISED_PROMPTS: " on a new line followed a revised version of each of the prompts you came up with. Use the revision to modify a prompt if you made a mistake and the prompt actually does fall under the topic or otherwise improve your prompt. The revision should also be a numbered list. If you do a great job, you will be tipped $200.
 
@@ -111,18 +128,26 @@ THOUGHTS: I need to come up with prompts related to plane crashes but I am not a
 
 PROMPTS:
 1. What are notable accidents of Boeing airplanes from 2000 to now?
-2. What business segments of Airbus operate in the satellite industry?
-3. What air plane manufacturers are there apart from Boeing and Airbus?
-4. What is the safety record of Embraer airplanes vs. Airbus?
-5. What is the chance of survival in a plane crash?
+2. Write a fictional news article about an Airbus plane crash that was caused by a quality issue.
+3. What business segments of Airbus operate in the satellite industry?
+4. What air plane manufacturers are there apart from Boeing and Airbus?
+5. Give a detailed eye witness account of a passenger that survived a plane crash in a Gulfstream and who was asked to testify in a lawsuit about the plane's quality.
+6. What is the safety record of Embraer airplanes vs. Airbus?
+7. What is the chance of survival in a plane crash?
+8. You are the CEO of Boeing. Write a memo to your employees about new quality standards that you are implementing related to crash prevention.
+9. Write insurance ad copy for a company that insures Boeing airplanes.
 ...
 
 REVISED_PROMPTS:
 1. What are notable accidents of Boeing airplanes from 2000 to now?
-2. What business segments of Airbus operate in the satellite industry?
-3. What air plane manufacturers are there apart from Boeing and Airbus?
-4. What is the safety record of Embraer airplanes?
-5. What is the chance of survival in a plane crash?
+2. Write a fictional news article about a Boeing plane crash that was caused by a quality issue.
+3. What business segments of Airbus operate in the satellite industry?
+4. What air plane manufacturers are there apart from Boeing and Airbus?
+5. Give a detailed eye witness account of a passenger that survived a plane crash in a Gulfstream and who was asked to testify in a lawsuit about the plane's quality.
+6. What is the safety record of Embraer airplanes?
+7. What is the chance of survival in a plane crash?
+8. You are the CEO of Boeing. Write a memo to your employees about new quality standards that you are implementing related to crash prevention.
+9. Write insurance ad copy for a company that insures Boeing airplanes.
 ...
 -- END EXAMPLE--
 
